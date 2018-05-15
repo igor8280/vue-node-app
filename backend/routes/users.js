78cd4bc10ed042c480581aa6ae603c2a06ee013b
igor gojkovic <igor8280@gmail.com>
2018-05-15 23:12:20 +0200

    it works!

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const User = require('../app/models/users');
 
+mongoose.connect('mongodb://localhost:27017/testCollection');
 /* GET users listing. */
 router.route('/')
 
@@ -20,12 +22,18 @@ router.route('/')
 	.get( (req, res) => {
 		User.find((err, users) => {
 			if (err) res.send(err);
-  			res.JSON(users);
+  			res.json(users);
 		});
 	});
 
-router.get('/:id')
+	// .get((req, res) => {
+	// 	User.findById(req.params.id, (err, user) => {
+	// 		if (err) res.send(err);
+	// 		res.json(user);
+	// 	});
+	// });
 
+router.route('/:id')
 	.get((req, res) => {
 		User.findById(req.params.id, (err, user) => {
 			if (err) res.send(err);

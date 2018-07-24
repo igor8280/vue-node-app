93b1dc1988a754a1e856ef5d65feea67f0e7ac26
Igor Gojkovic <igor8280@gmail.com>
2018-07-24 13:36:28 +0200

    file watcher

--- backend/src/config/config.index.js
+++ backend/src/config/config.index.js
@@ -2,18 +2,29 @@ const env = process.env.NODE_ENV || 'development';
 
 let mongoUrl = null;
 if (env === 'test')
-	mongoUrl = "mongodb://localhost:27017/proto-meta-test";
+	mongoUrl = 'mongodb://localhost:27017/proto-meta-test';
 else
-	mongoUrl = "mongodb://localhost:27017/proto-meta";
+	mongoUrl = 'mongodb://localhost:27017/proto-meta';
 
 const config = {
 	env,
-	"port": 3006,
+	'port': 3006,
 	mongoUrl,
-	"bodyLimit": "100kb",
-	"salt": {
-		"access_token": "sa-dFf?GfGdsa=4cJKvdFsa",
-		"refresh_token": "daS32=dGfa=-dsDf?dfa3"
+	'bodyLimit': '100kb',
+	'salt': {
+		'access_token': 'sa-dFf?GfGdsa=4cJKvdFsa',
+		'refresh_token': 'daS32=dGfa=-dsDf?dfa3'
+	},
+	pathPublicImages: {
+		'dotfiles': 'ignore',
+		'etag': false,
+		'extensions': ['png', 'jpg', 'jpeg'],
+		'index': false,
+		'maxAge': '1y',
+		'redirect': false,
+		setHeaders: function (res, path, stat) {
+			res.set('x-timestamp', Date.now())
+		}
 	}
 };
 

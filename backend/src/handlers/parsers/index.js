3b985097dfc4d065cb273ebeb67cdc699269309f
igor <igor8280@mail.com>
2018-07-27 15:06:29 +0200

    dynamic import

--- backend/src/handlers/parsers/index.js
+++ backend/src/handlers/parsers/index.js
@@ -1,31 +1,13 @@
-import FoxCrime from './fox_crime';
-import Parser1 from './parser1';
-import Parser2 from './parser2';
-
 class Parsers {
 	constructor() {}
 
-	// getParser(name) {
-	// 	if (name) {
-	// 		return import('./fox_crime/index.js').then(module => {
-	// 			console.log('module', module);
-	// 			return new module();
-	// 		}).catch(err => {
-	// 			console.log('eeeeeerrrrrrroooooorrrrrrr', err);
-	// 		});
-	// 	}
-	// }
-	
 	getParser(name) {
-		switch (name) {
-			case 'fox_crime':
-				return new FoxCrime();
-			case 'parser1':
-				return new Parser1();
-			case 'parser2':
-				return new Parser2();
-			default:
-				return {'error': true, code: 500, 'message': `There is no parser with name '${name}'`};
+		if (name) {
+			return import('./fox_crime/index.js').then(module => {
+				return new module.default();
+			}).catch(err => {
+				console.log('error', err);
+			});
 		}
 	}
 }
